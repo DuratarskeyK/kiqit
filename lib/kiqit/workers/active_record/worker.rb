f6fbfc67c6535ec36ce4fea6a1f5dd79b1005573
Wedge <shadowpilot34@gmail.com>
2016-06-03 13:39:54 +0300

    Final fix

--- lib/kiqit/workers/active_record/worker.rb
+++ lib/kiqit/workers/active_record/worker.rb
@@ -3,11 +3,13 @@ module Kiqit
     module ActiveRecord
       class Worker < Kiqit::Workers::Base
         def perform(klass, id, method, *args)
-          args         = Kiqit::ArgsParser.args_from_sidekiq(args)
-          runner_klass = klass.constantize
-          record       = runner_klass.find(id)
+          ::ActiveRecord::Base.connection_pool.with_connection do
+            args         = Kiqit::ArgsParser.args_from_sidekiq(args)
+            runner_klass = klass.constantize
+            record       = runner_klass.find(id)
           
-          perform_job(record, method, args)
+            perform_job(record, method, args)
+          end
         ensure
           ::ActiveRecord::Base.clear_active_connections!
           ::ActiveRecord::Base.connection.close
